fd05f51d964d9b012a711bf10b43755adbd7275f
Wanderson David <wandersondaviv.ifg@gmail.com>
2025-07-06 00:47:47 -0300

    feat: add Istiod Helm release configuration and related files

--- 1-isito-base.tf
+++ 1-isito-base.tf
@@ -1,16 +1,14 @@
-resource "helm_release" "istio-base" {
+resource "helm_release" "istio_base" {
   name = "my-istio-base-release"
 
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  chart      = "base"
-  namespace =  "istio-system"
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "base"
+  namespace        = "istio-system"
   create_namespace = true
-  version = "1.26.2"
-
+  version          = "1.26.2"
 
   set {
     name  = "global.istioNamespace"
     value = "istio-system"
   }
-
 }
---
+++ 2-isitod.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "istiod" {
+  name = "my-istiod-release"
+
+  repository       = "https://istio-release.storage.googleapis.com/charts"
+  chart            = "istiod"
+  namespace        = "istio-system"
+  create_namespace = true
+  version = "1.26.2"
+
+  set {
+    name  = "telemetry.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "global.istioNamespace"
+    value = "istio-system"
+  }
+
+  set {
+    name  = "meshConfig.ingressService"
+    value = "istio-gateway"
+  }
+
+  set {
+    name  = "meshConfig.ingressSelector"
+    value = "gateway"
+  }
+
+  depends_on = [helm_release.istio_base]
+}
